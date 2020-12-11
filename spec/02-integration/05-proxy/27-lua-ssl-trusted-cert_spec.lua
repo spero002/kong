spero002/kong

commit 1e6afa171272a12a1577c7f7e172a5a0cac4ca30

feat(conf) improved lua_ssl_trusted_certificate (#6342)
Adds two features to the `lua_ssl_trusted_certificate` option:

* It is now a comma-separated list instead of a single item. All the paths on the list are concatenated into a single file in a fixed location (`$PREFIX/.ca_combined`).
* It accepts a special system value, which is expanded to the "system default". This follows a very simple heuristic to try to use the most common cert in most popular distros. `system` can be used in conjunction with other paths.

Note that this PR involves modifying Kong's template - as a result, the `UPGRADE.md` file should include such changes when a new version is released.

kong/cmd/utils/prefix_handler.lua
@@ -103,6 +103,23 @@ local function gen_default_ssl_cert(kong_config, target)
   return true
 end
 
+
+local function gen_trusted_certs_combined_file(combined_filepath, paths)
+
+  log.verbose("generating trusted certs combined file in ",
+              combined_filepath)
+
+  local fd = assert(io.open(combined_filepath, "w"))
+
+  for _, path in ipairs(paths) do
+    fd:write(pl_file.read(path))
+    fd:write("\n")
+  end
+
+  io.close(fd)
+end
+
+
 local function get_ulimit()
   local ok, _, stdout, stderr = pl_utils.executeex "ulimit -n"
   if not ok then
@@ -302,6 +319,13 @@ local function prepare_prefix(kong_config, nginx_custom_template_path)
     end
     kong_config.status_ssl_cert = kong_config.status_ssl_cert_default
     kong_config.status_ssl_cert_key = kong_config.status_ssl_cert_key_default
+  end
+
+  if kong_config.lua_ssl_trusted_certificate_combined then
+    gen_trusted_certs_combined_file(
+      kong_config.lua_ssl_trusted_certificate_combined,
+      kong_config.lua_ssl_trusted_certificate
+    )
   end
 
   -- check ulimit

kong/conf_loader/init.lua
@@ -566,6 +566,7 @@ local CONF_INFERENCES = {
     deprecated = { replacement = false }
   },
 
+  lua_ssl_trusted_certificate = { typ = "array" },
   lua_ssl_verify_depth = { typ = "number" },
   lua_socket_pool_size = { typ = "number" },
 
@@ -801,11 +802,31 @@ local function check_and_infer(conf, opts)
     end
   end
 
-  if conf.lua_ssl_trusted_certificate and
-     not pl_path.exists(conf.lua_ssl_trusted_certificate)
-  then
-    errors[#errors + 1] = "lua_ssl_trusted_certificate: no such file at " ..
-                        conf.lua_ssl_trusted_certificate
+  if conf.lua_ssl_trusted_certificate then
+    local new_paths = {}
+
+    for i, path in ipairs(conf.lua_ssl_trusted_certificate) do
+      if path == "system" then
+        local system_path, err = utils.get_system_trusted_certs_filepath()
+        if system_path then
+          path = system_path
+
+        else
+          errors[#errors + 1] =
+            "lua_ssl_trusted_certificate: unable to locate system bundle - " ..
+            err
+        end
+      end
+
+      if not pl_path.exists(path) then
+        errors[#errors + 1] = "lua_ssl_trusted_certificate: no such file at " ..
+                               path
+      end
+
+      new_paths[i] = path
+    end
+
+    conf.lua_ssl_trusted_certificate = new_paths
   end
 
   if conf.ssl_cipher_suite ~= "custom" then
@@ -1513,9 +1534,13 @@ local function load(path, custom_conf, opts)
     conf.admin_ssl_cert_key = pl_path.abspath(conf.admin_ssl_cert_key)
   end
 
-  if conf.lua_ssl_trusted_certificate then
+  if conf.lua_ssl_trusted_certificate
+     and #conf.lua_ssl_trusted_certificate > 0 then
     conf.lua_ssl_trusted_certificate =
-      pl_path.abspath(conf.lua_ssl_trusted_certificate)
+      tablex.map(pl_path.abspath, conf.lua_ssl_trusted_certificate)
+
+    conf.lua_ssl_trusted_certificate_combined =
+      pl_path.abspath(pl_path.join(conf.prefix, ".ca_combined"))
   end
 
   if conf.cluster_cert and conf.cluster_cert_key then

spec/02-integration/05-proxy/27-lua-ssl-trusted-cert_spec.lua
@@ -1,0 +1,78 @@
+local helpers = require "spec.helpers"
+
+for _, strategy in helpers.each_strategy() do
+  local bp
+
+  describe("lua_ssl_trusted_cert #" .. strategy, function()
+    before_each(function()
+      bp = helpers.get_db_utils(strategy, {
+        "routes",
+        "plugins",
+      })
+
+      local r = bp.routes:insert({ hosts = {"test.dev"} })
+
+      bp.plugins:insert({
+        name = "pre-function",
+        route = { id = r.id },
+        config = {
+          access = {
+            string.format([[
+                local tcpsock = ngx.socket.tcp()
+                assert(tcpsock:connect("%s", %d))
+
+                assert(tcpsock:sslhandshake(
+                  nil,         -- reused_session
+                  nil,         -- server_name
+                  true         -- ssl_verify
+                ))
+
+                assert(tcpsock:close())
+              ]],
+              helpers.mock_upstream_ssl_host,
+              helpers.mock_upstream_ssl_port
+            )
+          },
+        },
+      })
+    end)
+
+    after_each(function()
+      assert(helpers.stop_kong())
+    end)
+
+    it("works with single entry", function()
+      assert(helpers.start_kong({
+        database   = strategy,
+        nginx_conf = "spec/fixtures/custom_nginx.template",
+        lua_ssl_trusted_certificate = "spec/fixtures/kong_spec.crt",
+      }))
+
+      local proxy_client = helpers.proxy_client()
+
+      local res = proxy_client:get("/", {
+        headers = { host = "test.dev" },
+      })
+      assert.res_status(200, res)
+    end)
+
+    it("works with multiple entries", function()
+      assert(helpers.start_kong({
+        database   = strategy,
+        nginx_conf = "spec/fixtures/custom_nginx.template",
+        lua_ssl_trusted_certificate = "spec/fixtures/kong_clustering_ca.crt,spec/fixtures/kong_clustering.crt",
+        ssl_cert = "spec/fixtures/kong_clustering.crt",
+        ssl_cert_key = "spec/fixtures/kong_clustering.key",
+      }))
+
+      local proxy_client = helpers.proxy_client()
+
+      local res = proxy_client:get("/", {
+        headers = { host = "test.dev" },
+      })
+      assert.res_status(200, res)
+    end)
+  end)
+end
+
+

kong.conf.default
@@ -1224,12 +1224,32 @@
 # See the lua-nginx-module documentation for more information:
 # https://github.com/openresty/lua-nginx-module
 
-#lua_ssl_trusted_certificate =   # Absolute path to the certificate
-                                 # authority file for Lua cosockets in PEM
-                                 # format. This certificate will be the one
-                                 # used for verifying Kong's database
-                                 # connections, when `pg_ssl_verify` or
-                                 # `cassandra_ssl_verify` are enabled.
+
+#lua_ssl_trusted_certificate =   # Comma-separated list of paths to certificate
+                                 # authority files for Lua cosockets in PEM format.
+                                 #
+                                 # The special value `system` attempts to search for the
+                                 # "usual default" provided by each distro, according
+                                 # to an arbitrary heuristic. In the current implementation,
+                                 # The following pathnames will be tested in order,
+                                 # and the first one found will be used:
+                                 #
+                                 # * /etc/ssl/certs/ca-certificates.crt (Debian/Ubuntu/Gentoo)
+                                 # * /etc/pki/tls/certs/ca-bundle.crt (Fedora/RHEL 6)
+                                 # * /etc/ssl/ca-bundle.pem (OpenSUSE)
+                                 # * /etc/pki/tls/cacert.pem (OpenELEC)
+                                 # * /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem (CentOS/RHEL 7)
+                                 # * /etc/ssl/cert.pem (OpenBSD, Alpine)
+                                 #
+                                 # If no file is found on any of these paths, an error will
+                                 # be raised.
+                                 #
+                                 # `system` can be used by itself or in conjunction with other
+                                 # CA filepaths.
+                                 #
+                                 # When `pg_ssl_verify` or `cassandra_ssl_verify`
+                                 # are enabled, these certificate authority files will be
+                                 # used for verifying Kong's database connections.
                                  #
                                  # See https://github.com/openresty/lua-nginx-module#lua_ssl_trusted_certificate
 

kong/db/strategies/cassandra/connector.lua
@@ -141,7 +141,7 @@ function CassandraConnector.new(kong_config)
     max_schema_consensus_wait = kong_config.cassandra_schema_consensus_timeout,
     ssl                       = kong_config.cassandra_ssl,
     verify                    = kong_config.cassandra_ssl_verify,
-    cafile                    = kong_config.lua_ssl_trusted_certificate,
+    cafile                    = kong_config.lua_ssl_trusted_certificate_combined,
     lock_timeout              = 30,
     silent                    = ngx.IS_CLI,
   }

kong/db/strategies/postgres/connector.lua
@@ -925,7 +925,7 @@ function _M.new(kong_config)
     schema      = kong_config.pg_schema or "",
     ssl         = kong_config.pg_ssl,
     ssl_verify  = kong_config.pg_ssl_verify,
-    cafile      = kong_config.lua_ssl_trusted_certificate,
+    cafile      = kong_config.lua_ssl_trusted_certificate_combined,
     sem_max     = kong_config.pg_max_concurrent_queries or 0,
     sem_timeout = (kong_config.pg_semaphore_timeout or 60000) / 1000,
   }
@@ -962,7 +962,7 @@ function _M.new(kong_config)
       schema      = kong_config.pg_ro_schema,
       ssl         = kong_config.pg_ro_ssl,
       ssl_verify  = kong_config.pg_ro_ssl_verify,
-      cafile      = kong_config.lua_ssl_trusted_certificate,
+      cafile      = kong_config.lua_ssl_trusted_certificate_combined,
       sem_max     = kong_config.pg_ro_max_concurrent_queries,
       sem_timeout = kong_config.pg_ro_semaphore_timeout and
                     (kong_config.pg_ro_semaphore_timeout / 1000) or nil,

kong/templates/nginx_kong.lua
@@ -11,8 +11,8 @@ lua_socket_log_errors  off;
 lua_max_running_timers 4096;
 lua_max_pending_timers 16384;
 lua_ssl_verify_depth   ${{LUA_SSL_VERIFY_DEPTH}};
-> if lua_ssl_trusted_certificate then
-lua_ssl_trusted_certificate '${{LUA_SSL_TRUSTED_CERTIFICATE}}';
+> if lua_ssl_trusted_certificate_combined then
+lua_ssl_trusted_certificate '${{LUA_SSL_TRUSTED_CERTIFICATE_COMBINED}}';
 > end
 
 lua_shared_dict kong                        5m;

kong/templates/nginx_kong_stream.lua
@@ -11,8 +11,8 @@ lua_socket_log_errors  off;
 lua_max_running_timers 4096;
 lua_max_pending_timers 16384;
 lua_ssl_verify_depth   ${{LUA_SSL_VERIFY_DEPTH}};
-> if lua_ssl_trusted_certificate then
-lua_ssl_trusted_certificate '${{LUA_SSL_TRUSTED_CERTIFICATE}}';
+> if lua_ssl_trusted_certificate_combined then
+lua_ssl_trusted_certificate '${{LUA_SSL_TRUSTED_CERTIFICATE_COMBINED}}';
 > end
 
 lua_shared_dict stream_kong                        5m;

commit 9042e0c6680bd685505c576e83cc5b207b59618a

fix(conf) combine trusted certs in conf_loader
The commit 1e6afa171272a12a1577c7f7e172a5a0cac4ca30 introduced a regression for a case where we want to enable verification of database certificates when doing SSL handshake connections. For example, if we wanted to specify `pg_ssl = on` and `pg_ssl_verify = on` to enable verification of the postgres certificate, the following error would be thrown:

```
Error:
/usr/local/share/lua/5.1/kong/cmd/migrations.lua:220: [PostgreSQL error] failed to retrieve PostgreSQL server_version_num: error loading CA locations (No such file or directory)
stack traceback:
[C]: in function 'assert'
/usr/local/share/lua/5.1/kong/cmd/migrations.lua:220: in function 'cmd_exec'
/usr/local/share/lua/5.1/kong/cmd/init.lua:96: in function </usr/local/share/lua/5.1/kong/cmd/init.lua:96>
[C]: in function 'xpcall'
/usr/local/share/lua/5.1/kong/cmd/init.lua:96: in function </usr/local/share/lua/5.1/kong/cmd/init.lua:53>
/usr/local/bin/kong:9: in function 'file_gen'
init_worker_by_lua:48: in function <init_worker_by_lua:46>
[C]: in function 'xpcall'
init_worker_by_lua:55: in function <init_worker_by_lua:53>
```

The aforementioned commit added support for configuring a list of `lua_ssl_trusted_certificate` and the paths on the list are concatenated into a single file in a fixed location (`$PREFIX/.ca_combined`). The configuration used for enabling verification of database certificates was defined by the new `lua_ssl_trusted_certificate_combined` property.

But the problem was that the generation of the combined file containing the trusted certificates was done in the `prefix_handler` module. Commands such as `kong migrations bootstrap` would fail because they don't use the `prefix_handler` module. The correct place should be in `conf_loader` where the `lua_ssl_trusted_certificate_combined` is first defined.

kong/cmd/utils/prefix_handler.lua
@@ -104,22 +104,6 @@ local function gen_default_ssl_cert(kong_config, target)
 end
 
 
-local function gen_trusted_certs_combined_file(combined_filepath, paths)
-
-  log.verbose("generating trusted certs combined file in ",
-              combined_filepath)
-
-  local fd = assert(io.open(combined_filepath, "w"))
-
-  for _, path in ipairs(paths) do
-    fd:write(pl_file.read(path))
-    fd:write("\n")
-  end
-
-  io.close(fd)
-end
-
-
 local function get_ulimit()
   local ok, _, stdout, stderr = pl_utils.executeex "ulimit -n"
   if not ok then
@@ -319,13 +303,6 @@ local function prepare_prefix(kong_config, nginx_custom_template_path)
     end
     kong_config.status_ssl_cert = kong_config.status_ssl_cert_default
     kong_config.status_ssl_cert_key = kong_config.status_ssl_cert_key_default
-  end
-
-  if kong_config.lua_ssl_trusted_certificate_combined then
-    gen_trusted_certs_combined_file(
-      kong_config.lua_ssl_trusted_certificate_combined,
-      kong_config.lua_ssl_trusted_certificate
-    )
   end
 
   -- check ulimit

kong/conf_loader/init.lua
@@ -1165,6 +1165,22 @@ local function load_config_file(path)
 end
 
 
+local function gen_trusted_certs_combined_file(combined_filepath, paths)
+
+  log.verbose("generating trusted certs combined file in ",
+              combined_filepath)
+
+  local fd = assert(io.open(combined_filepath, "w"))
+
+  for _, path in ipairs(paths) do
+    fd:write(pl_file.read(path))
+    fd:write("\n")
+  end
+
+  io.close(fd)
+end
+
+
 --- Load Kong configuration
 -- The loaded configuration will have all properties from the default config
 -- merged with the (optionally) specified config file, environment variables
@@ -1541,6 +1557,11 @@ local function load(path, custom_conf, opts)
 
     conf.lua_ssl_trusted_certificate_combined =
       pl_path.abspath(pl_path.join(conf.prefix, ".ca_combined"))
+
+    gen_trusted_certs_combined_file(
+      conf.lua_ssl_trusted_certificate_combined,
+      conf.lua_ssl_trusted_certificate
+    )
   end
 
   if conf.cluster_cert and conf.cluster_cert_key then

spec/02-integration/05-proxy/27-lua-ssl-trusted-cert_spec.lua
@@ -2,6 +2,7 @@ local helpers = require "spec.helpers"
 
 for _, strategy in helpers.each_strategy() do
   local bp
+  local postgres_only = strategy == "postgres" and it or pending
 
   describe("lua_ssl_trusted_cert #" .. strategy, function()
     before_each(function()
@@ -38,7 +39,7 @@ for _, strategy in helpers.each_strategy() do
     end)
 
     after_each(function()
-      assert(helpers.stop_kong())
+      helpers.stop_kong()
     end)
 
     it("works with single entry", function()
@@ -71,6 +72,18 @@ for _, strategy in helpers.each_strategy() do
         headers = { host = "test.dev" },
       })
       assert.res_status(200, res)
+    end)
+
+    postgres_only("works with SSL verification", function()
+      local _, err = helpers.start_kong({
+        database   = strategy,
+        nginx_conf = "spec/fixtures/custom_nginx.template",
+        lua_ssl_trusted_certificate = "spec/fixtures/kong_clustering_ca.crt,spec/fixtures/kong_clustering.crt",
+        pg_ssl = "on",
+        pg_ssl_verify = "on",
+      })
+
+      assert.not_matches("error loading CA locations %(No such file or directory%)", err)
     end)
   end)
 end